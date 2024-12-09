5facc7b234de0bbdbc7fb9c622a69d0259c20c16
Yannick Stade <100073938+ystade@users.noreply.github.com>
2024-12-09 17:06:20 +0100

    🎨 Update semantics in both devices

--- examples/device/c/device.c
+++ examples/device/c/device.c
@@ -303,18 +303,23 @@ int C_QDMI_query_operation_property_dev(C_QDMI_Operation operation,
 int C_QDMI_control_create_job_dev(const QDMI_Program_Format format,
                                   const size_t size, const void *prog,
                                   C_QDMI_Job *job) {
+  if (format != QDMI_PROGRAM_FORMAT_QASM2 &&
+      format != QDMI_PROGRAM_FORMAT_QIRBASESTRING &&
+      format != QDMI_PROGRAM_FORMAT_QIRBASEMODULE) {
+    if (prog != NULL && (size <= 0 || job == NULL)) {
+      return QDMI_ERROR_INVALIDARGUMENT;
+    }
+    return QDMI_ERROR_NOTSUPPORTED;
+  }
+  if (prog == NULL) {
+    return QDMI_SUCCESS;
+  }
+  if (size == 0 || job == NULL) {
+    return QDMI_ERROR_INVALIDARGUMENT;
+  }
   if (C_QDMI_read_device_status() != QDMI_DEVICE_STATUS_IDLE) {
     return QDMI_ERROR_FATAL;
   }
-  if (size == 0 || prog == NULL || job == NULL) {
-    return QDMI_ERROR_INVALIDARGUMENT;
-  }
-  if (format != QDMI_PROGRAM_FORMAT_QASM2 &&
-      format != QDMI_PROGRAM_FORMAT_QIRSTRING &&
-      format != QDMI_PROGRAM_FORMAT_QIRMODULE) {
-    return QDMI_ERROR_NOTSUPPORTED;
-  }
-
   *job = (C_QDMI_Job)malloc(sizeof(C_QDMI_Job_impl_t));
   // set job id to random number for demonstration purposes
   (*job)->id = rand();
