aa4eb24097e6fbe5f2d88605429bcafa172cfc3b
burgholzer <burgholzer@users.noreply.github.com>
2025-01-20 07:41:22 +0000

    Remove preview for PR 135 🛬

--- pr-preview/pr-135/search/all_a.js
+++
@@ -1,4 +0,0 @@
-var searchData=
-[
-  ['kinds_20of_20jobs_20for_20devices_20and_20clients_0',['Why are there separate kinds of jobs for devices and clients?',['../md_docs_2rationale.html#rationale-job-structs',1,'']]]
-];
