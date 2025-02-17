89102421811b9870628f99dd51240f37f03512ac
burgholzer <burgholzer@me.com>
2025-02-17 11:15:07 +0000

    Deploying to gh-pages from @ Munich-Quantum-Software-Stack/QDMI@2aae32a90b9da921734c9c05fe24434f9aa54f59 🚀

--- navtreedata.js
+++ navtreedata.js
@@ -75,5 +75,5 @@ var NAVTREEINDEX =
 "client_8h.html"
 ];
 
-var SYNCONMSG = 'click to disable panel synchronisation';
-var SYNCOFFMSG = 'click to enable panel synchronisation';
+var SYNCONMSG = 'click to disable panel synchronization';
+var SYNCOFFMSG = 'click to enable panel synchronization';
