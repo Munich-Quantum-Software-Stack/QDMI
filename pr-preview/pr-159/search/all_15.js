c59070f7f709ecf9954d8df2b5af48610ba7513d
burgholzer <burgholzer@users.noreply.github.com>
2025-04-17 20:22:41 +0000

    Deploy preview for PR 159 🛫

---
+++ pr-preview/pr-159/search/all_15.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['why_20are_20some_20enum_20definitions_20placed_20in_20the_20tt_20constants_20h_20tt_20header_20and_20some_20are_20not_0',['Why are some enum definitions placed in the &lt;tt&gt;constants.h&lt;/tt&gt; header and some are not?',['../md_docs_2rationale.html#rationale-enum-definitions',1,'']]],
+  ['why_20are_20there_20separate_20kinds_20of_20jobs_20for_20devices_20and_20clients_1',['Why are there separate kinds of jobs for devices and clients?',['../md_docs_2rationale.html#rationale-job-structs',1,'']]],
+  ['why_20do_20device_20implementations_20use_20a_20prefix_2',['Why do device implementations use a prefix?',['../md_docs_2rationale.html#rationale-prefix',1,'']]],
+  ['why_20do_20devices_20have_20sessions_3',['Why do devices have sessions?',['../md_docs_2rationale.html#device-session',1,'']]],
+  ['why_20do_20sessions_20need_20to_20be_20initialized_20after_20allocation_4',['Why do sessions need to be initialized after allocation?',['../md_docs_2rationale.html#rationale-session-init',1,'']]],
+  ['why_20does_20qdmi_20not_20define_20individual_20functions_20for_20each_20property_5',['Why does QDMI not define individual functions for each property?',['../md_docs_2rationale.html#rationale-properties',1,'']]],
+  ['why_20does_20qdmi_20use_20opaque_20pointers_6',['Why does QDMI use opaque pointers?',['../md_docs_2rationale.html#rationale-opaque-pointers',1,'']]],
+  ['with_20the_20template_7',['Working with the Template',['../md_docs_2templates.html#template-working',1,'']]],
+  ['working_20with_20the_20template_8',['Working with the Template',['../md_docs_2templates.html#template-working',1,'']]]
+];
---
+++ pr-preview/pr-159/search/all_2.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['client_20interface_0',['QDMI Client Interface',['../group__client__interface.html',1,'']]],
+  ['client_20job_20interface_1',['QDMI Client Job Interface',['../group__client__job__interface.html',1,'']]],
+  ['client_20query_20interface_2',['QDMI Client Query Interface',['../group__client__query__interface.html',1,'']]],
+  ['client_20session_20interface_3',['QDMI Client Session Interface',['../group__client__session__interface.html',1,'']]],
+  ['client_2eh_4',['client.h',['../client_8h.html',1,'']]],
+  ['clients_5',['Why are there separate kinds of jobs for devices and clients?',['../md_docs_2rationale.html#rationale-job-structs',1,'']]],
+  ['complex_20properties_6',['Complex Properties',['../md_docs_2examples.html#device-complex',1,'']]],
+  ['configuring_20the_20template_7',['Configuring the Template',['../md_docs_2templates.html#template-configure',1,'']]],
+  ['constants_20h_20tt_20header_20and_20some_20are_20not_8',['Why are some enum definitions placed in the &lt;tt&gt;constants.h&lt;/tt&gt; header and some are not?',['../md_docs_2rationale.html#rationale-enum-definitions',1,'']]],
+  ['constants_2eh_9',['constants.h',['../constants_8h.html',1,'']]],
+  ['creating_20a_20new_20project_10',['Creating a new Project',['../md_docs_2templates.html#template-create',1,'']]]
+];
