d17523840316e1868541a3a581be43561bdbc02a
burgholzer <burgholzer@users.noreply.github.com>
2025-04-26 19:51:50 +0000

    Remove preview for PR 159 🛬

--- pr-preview/pr-159/search/all_14.js
+++
@@ -1,5 +0,0 @@
-var searchData=
-[
-  ['use_20a_20prefix_0',['Why do device implementations use a prefix?',['../md_docs_2rationale.html#rationale-prefix',1,'']]],
-  ['use_20opaque_20pointers_1',['Why does QDMI use opaque pointers?',['../md_docs_2rationale.html#rationale-opaque-pointers',1,'']]]
-];
