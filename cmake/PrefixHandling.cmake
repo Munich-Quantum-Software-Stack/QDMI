d34e3878828b9ade2106b7d6b917b1107db5c663
Lukas Burgholzer <burgholzer@me.com>
2025-07-03 15:24:15 +0200

    Apply suggestions from code review
    Signed-off-by: Lukas Burgholzer <burgholzer@me.com>

--- cmake/PrefixHandling.cmake
+++ cmake/PrefixHandling.cmake
@@ -75,5 +75,4 @@ function(generate_device_defs_executable prefix)
     PRIVATE qdmi::qdmi qdmi::${QDMI_prefix}_device qdmi::project_warnings)
   target_compile_features(qdmi_test_${QDMI_prefix}_device_defs
                           PRIVATE cxx_std_17)
-  target_compile_options(qdmi_test_${QDMI_prefix}_device_defs PRIVATE -pedantic)
 endfunction()
