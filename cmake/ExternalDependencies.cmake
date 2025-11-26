e0c85519b7384fe220ecee33a7cccca42b05445a
burgholzer <burgholzer@me.com>
2025-11-26 10:12:45 +0100

    📝 Re-enable FetchContent for doxygen and build docs by default
    Signed-off-by: burgholzer <burgholzer@me.com>

--- cmake/ExternalDependencies.cmake
+++ cmake/ExternalDependencies.cmake
@@ -21,59 +21,35 @@ include(CMakeDependentOption)
 set(FETCH_PACKAGES "")
 
 if(BUILD_QDMI_DOCS)
-  # Not using FetchContent here, as the source build of doxygen is currently
-  # broken. See https://github.com/doxygen/doxygen/issues/11416 for further
-  # details.
-  find_package(Doxygen 1.12.0 REQUIRED)
-  # cmake-format: off
-  #  set(DOXYGEN_VERSION
-  #      1.12.0
-  #      CACHE STRING "Doxygen version")
-  #  set(DOXYGEN_REV
-  #      "64dfee0a4b65a4dc3687dfc6b31535a844681ffa"
-  #      CACHE STRING "Doxygen identifier (tag, branch or commit hash)")
-  #  if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
-  #    FetchContent_Declare(
-  #      Doxygen
-  #      GIT_REPOSITORY https://github.com/doxygen/doxygen.git
-  #      GIT_TAG ${DOXYGEN_REV}
-  #      FIND_PACKAGE_ARGS ${DOXYGEN_VERSION})
-  #    list(APPEND FETCH_PACKAGES Doxygen)
-  #  else()
-  #    find_package(Doxygen ${DOXYGEN_VERSION} QUIET)
-  #    if(NOT Doxygen_FOUND)
-  #      FetchContent_Declare(
-  #        Doxygen
-  #        GIT_REPOSITORY https://github.com/doxygen/doxygen.git
-  #        GIT_TAG ${DOXYGEN_REV})
-  #      list(APPEND FETCH_PACKAGES Doxygen)
-  #    endif()
-  #  endif()
-  # cmake-format: on
+  set(CMAKE_POLICY_DEFAULT_CMP0116
+      NEW
+      CACHE STRING
+            "Set the default CMP0116 policy to NEW for documentation builds")
+  set(DOXYGEN_VERSION
+      1.15.0
+      CACHE STRING "Doxygen version")
+  set(DOXYGEN_REV
+      "7cca38ba5185457e6d9495bf963d4cdeacebc25a"
+      CACHE STRING "Doxygen identifier (tag, branch or commit hash)")
+  FetchContent_Declare(
+    Doxygen
+    GIT_REPOSITORY https://github.com/doxygen/doxygen.git
+    GIT_TAG ${DOXYGEN_REV}
+    FIND_PACKAGE_ARGS ${DOXYGEN_VERSION})
+  list(APPEND FETCH_PACKAGES Doxygen)
 
   set(DOXYGEN_AWESOME_VERSION
-      1.12.0
+      2.4.1
       CACHE STRING "Doxygen Awesome version")
   set(DOXYGEN_AWESOME_REV
-      "af1d9030b3ffa7b483fa9997a7272fb12af6af4c"
+      "1f3620084ff75734ed192101acf40e9dff01d848"
       CACHE STRING "Doxygen Awesome identifier (tag, branch or commit hash)")
-  if(CMAKE_VERSION VERSION_GREATER_EQUAL 3.24)
-    FetchContent_Declare(
-      doxygen-awesome-css
-      GIT_REPOSITORY https://github.com/jothepro/doxygen-awesome-css.git
-      GIT_TAG ${DOXYGEN_AWESOME_REV}
-      FIND_PACKAGE_ARGS ${DOXYGEN_AWESOME_VERSION})
-    list(APPEND FETCH_PACKAGES doxygen-awesome-css)
-  else()
-    find_package(doxygen-awesome-css ${DOXYGEN_AWESOME_VERSION} QUIET)
-    if(NOT doxygen-awesome-css_FOUND)
-      FetchContent_Declare(
-        doxygen-awesome-css
-        GIT_REPOSITORY https://github.com/jothepro/doxygen-awesome-css.git
-        GIT_TAG ${DOXYGEN_AWESOME_REV})
-      list(APPEND FETCH_PACKAGES doxygen-awesome-css)
-    endif()
-  endif()
+  FetchContent_Declare(
+    doxygen-awesome-css
+    GIT_REPOSITORY https://github.com/jothepro/doxygen-awesome-css.git
+    GIT_TAG ${DOXYGEN_AWESOME_REV}
+    FIND_PACKAGE_ARGS ${DOXYGEN_AWESOME_VERSION})
+  list(APPEND FETCH_PACKAGES doxygen-awesome-css)
 endif()
 
 if(BUILD_QDMI_TESTS)
