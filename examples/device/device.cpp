52c5501af1a30b31aae857bf40eb16f39a3cd86d
burgholzer <burgholzer@me.com>
2025-12-10 13:29:16 +0100

    🚸 Provide compile definitions for inferring device and QDMI versions automatically
    Signed-off-by: burgholzer <burgholzer@me.com>

--- examples/device/device.cpp
+++ examples/device/device.cpp
@@ -757,9 +757,11 @@ int CXX_QDMI_device_session_query_device_property(
   }
   ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_NAME, "C++ Device with 5 qubits",
                       prop, size, value, size_ret)
-  ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_VERSION, "0.1.0", prop, size, value,
-                      size_ret)
-  ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_LIBRARYVERSION, "1.3.0-dev", prop,
+  // NOLINTNEXTLINE(misc-include-cleaner)
+  ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_VERSION, DEVICE_VERSION, prop, size,
+                      value, size_ret)
+  // NOLINTNEXTLINE(misc-include-cleaner)
+  ADD_STRING_PROPERTY(QDMI_DEVICE_PROPERTY_LIBRARYVERSION, QDMI_VERSION, prop,
                       size, value, size_ret)
   ADD_SINGLE_VALUE_PROPERTY(QDMI_DEVICE_PROPERTY_STATUS, QDMI_Device_Status,
                             CXX_QDMI_get_device_status(), prop, size, value,
