5f4b5afff1224711f772d0ae1d5683718e95174c
burgholzer <burgholzer@users.noreply.github.com>
2025-12-20 20:03:11 +0000

    Deploy preview for PR 284 🛫

---
+++ pr-preview/pr-284/files_dup.js
@@ -0,0 +1,7 @@
+var files_dup =
+[
+    [ "client.h", "client_8h.html", "client_8h" ],
+    [ "constants.h", "constants_8h.html", "constants_8h" ],
+    [ "device.h", "device_8h.html", "device_8h" ],
+    [ "types.h", "types_8h.html", "types_8h" ]
+];
