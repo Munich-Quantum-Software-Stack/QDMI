02d583ae500197cc6e5a536dd2e514f87808ebaa
Yannick Stade <100073938+ystade@users.noreply.github.com>
2026-02-15 16:27:10 +0100

    🎨 Install All Header into Device (#325)
    ## Description
    
    Only two of the three headers necessary for the device interface are
    prefixed. The `constants.h` is not. However, only due to this fact,
    every device depends on an available QDMI installation. This PR also
    adds the `constants.h` header to the files that are copied into the
    include directory of the device.
    
    ## Checklist:
    
    <!---
    This checklist serves as a reminder of a couple of things that ensure
    your pull request will be merged swiftly.
    -->
    
    - [x] The pull request only contains commits that are focused and
    relevant to this change.
    - [x] I have added appropriate tests that cover the new/changed
    functionality.
    - [ ] I have updated the documentation to reflect these changes.
    - [x] I have added entries to the changelog for any noteworthy
    additions, changes, fixes, or
          removals.
    - [ ] I have added migration instructions to the upgrade guide (if
    needed).
    - [x] The changes follow the project's style guidelines and introduce no
    new warnings.
    - [x] The changes are fully tested and pass the CI checks.
    - [x] I have reviewed my own code changes.
    
    ---------
    
    Signed-off-by: burgholzer <burgholzer@me.com>
    Co-authored-by: pre-commit-ci[bot] <66853113+pre-commit-ci[bot]@users.noreply.github.com>
    Co-authored-by: burgholzer <burgholzer@me.com>

--- cmake/PrefixHandling.cmake
+++ cmake/PrefixHandling.cmake
@@ -31,8 +31,8 @@ function(generate_prefixed_qdmi_headers prefix)
   endif()
 
   # Get the list of all QDMI device headers.
-  file(GLOB_RECURSE QDMI_DEVICE_HEADERS ${QDMI_INCLUDE_DIR}/qdmi/device.h
-       ${QDMI_INCLUDE_DIR}/qdmi/types.h)
+  file(GLOB_RECURSE QDMI_DEVICE_HEADERS ${QDMI_INCLUDE_DIR}/qdmi/constants.h
+       ${QDMI_INCLUDE_DIR}/qdmi/device.h ${QDMI_INCLUDE_DIR}/qdmi/types.h)
 
   # Determine the correct CMake directory for prefix_defs.txt
   set(QDMI_PREFIX_DIR "${QDMI_CMAKE_DIR}")
@@ -55,7 +55,7 @@ function(generate_prefixed_qdmi_headers prefix)
     # Replace the include for the device header with the prefixed version.
     string(
       REGEX
-      REPLACE "#include (\"|<)qdmi/(device|types).h(\"|>)"
+      REPLACE "#include (\"|<)qdmi/(constants|device|types).h(\"|>)"
               "#include \\1${QDMI_prefix}_qdmi/\\2.h\\3" header_content
               "${header_content}")
     # Replace the prefix definitions.
