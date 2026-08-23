35de8d204273dad1ad8a058adb22268d78368e98
burgholzer <burgholzer@users.noreply.github.com>
2026-08-23 19:57:54 +0000

    Deploy preview for PR 509 🛫

---
+++ pr-preview/pr-509/search/all_14.js
@@ -0,0 +1,14 @@
+var searchData=
+[
+  ['placed_20in_20the_20constants_20h_20header_20and_20some_20are_20not_0',['Why are some enum definitions placed in the &lt;span class=&quot;tt&quot;&gt;constants.h&lt;/span&gt; header and some are not?',['../md_docs_2rationale.html#rationale-enum-definitions',1,'']]],
+  ['pointers_1',['Why does QDMI use opaque pointers?',['../md_docs_2rationale.html#rationale-opaque-pointers',1,'']]],
+  ['prefix_2',['Why do device implementations use a prefix?',['../md_docs_2rationale.html#rationale-prefix',1,'']]],
+  ['principles_3',['Core Principles',['../md_docs_2ai__usage.html#core-principles',1,'']]],
+  ['profile_4',['profile',['../structQDMI__Program__Format.html#a3181aac9e7b3d398ccf856e0f9718fa6',1,'QDMI_Program_Format']]],
+  ['program_20format_20execution_20features_5',['Program-Format Execution Features',['../md_docs_2examples.html#device-program-format-features',1,'']]],
+  ['prohibited_20uses_6',['Prohibited Uses',['../md_docs_2ai__usage.html#prohibited-uses',1,'']]],
+  ['project_7',['Creating a new Project',['../md_docs_2templates.html#template-create',1,'']]],
+  ['properties_8',['Properties',['../md_docs_2examples.html#device-string',1,'Basic String Properties'],['../md_docs_2examples.html#device-complex',1,'Complex Properties'],['../md_docs_2examples.html#device-int-enumeration',1,'Integer or Enumeration Properties'],['../md_docs_2examples.html#device-list',1,'List Properties']]],
+  ['property_9',['Why does QDMI not define individual functions for each property?',['../md_docs_2rationale.html#rationale-properties',1,'']]],
+  ['pull_20request_20workflow_10',['Pull Request Workflow',['../md_docs_2contributing.html#pull-request-workflow',1,'']]]
+];
---
+++ pr-preview/pr-509/structQDMI__Program__Feature.js
@@ -0,0 +1,7 @@
+var structQDMI__Program__Feature =
+[
+    [ "id", "structQDMI__Program__Feature.html#af352c3f325e3cae93c6f6ad99c604bd7", null ],
+    [ "value", "structQDMI__Program__Feature.html#a9a00d1c9c44c9c3e70c7912c5bbdea50", null ],
+    [ "constraint_id", "structQDMI__Program__Feature.html#a88c2a6e70460719452dc9326f3a63bc2", null ],
+    [ "constraint_value", "structQDMI__Program__Feature.html#adc741495bd0c181716bcd9f75a7050ff", null ]
+];
