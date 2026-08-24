9dcac118c029eaa21d172c44ac179d91e9c9706e
burgholzer <burgholzer@users.noreply.github.com>
2026-08-24 11:09:01 +0000

    Deploy preview for PR 511 🛫

---
+++ pr-preview/pr-511/search/pages_8.js
@@ -0,0 +1,19 @@
+var searchData=
+[
+  ['define_20individual_20functions_20for_20each_20property_0',['Why does QDMI not define individual functions for each property?',['../md_docs_2rationale.html#rationale-properties',1,'']]],
+  ['definitions_20placed_20in_20the_20constants_20h_20header_20and_20some_20are_20not_1',['Why are some enum definitions placed in the &lt;span class=&quot;tt&quot;&gt;constants.h&lt;/span&gt; header and some are not?',['../md_docs_2rationale.html#rationale-enum-definitions',1,'']]],
+  ['device_2',['Implementing a Device',['../md_docs_2examples.html#device',1,'']]],
+  ['device_20implementations_20use_20a_20prefix_3',['Why do device implementations use a prefix?',['../md_docs_2rationale.html#rationale-prefix',1,'']]],
+  ['devices_20and_20clients_4',['Why are there separate kinds of jobs for devices and clients?',['../md_docs_2rationale.html#rationale-job-structs',1,'']]],
+  ['devices_20have_20sessions_5',['Why do devices have sessions?',['../md_docs_2rationale.html#device-session',1,'']]],
+  ['different_20kinds_20of_20sites_6',['Why are there different kinds of sites?',['../md_docs_2rationale.html#rationale-site-types',1,'']]],
+  ['disclosure_7',['4. Transparency and Disclosure',['../md_docs_2ai__usage.html#transparency-and-disclosure',1,'']]],
+  ['do_20device_20implementations_20use_20a_20prefix_8',['Why do device implementations use a prefix?',['../md_docs_2rationale.html#rationale-prefix',1,'']]],
+  ['do_20devices_20have_20sessions_9',['Why do devices have sessions?',['../md_docs_2rationale.html#device-session',1,'']]],
+  ['do_20sessions_20need_20to_20be_20initialized_20after_20allocation_10',['Why do sessions need to be initialized after allocation?',['../md_docs_2rationale.html#rationale-session-init',1,'']]],
+  ['documentation_11',['Documentation',['../md_docs_2contributing.html#building-the-documentation',1,'Building the Documentation'],['../md_docs_2contributing.html#working-on-the-documentation',1,'Working on the Documentation']]],
+  ['does_20qdmi_20not_20define_20individual_20functions_20for_20each_20property_12',['Why does QDMI not define individual functions for each property?',['../md_docs_2rationale.html#rationale-properties',1,'']]],
+  ['does_20qdmi_20use_20opaque_20pointers_13',['Why does QDMI use opaque pointers?',['../md_docs_2rationale.html#rationale-opaque-pointers',1,'']]],
+  ['driver_14',['Implementing a Client Driver',['../md_docs_2examples.html#client-driver',1,'']]],
+  ['dynamic_20content_15',['Dynamic Content',['../md_docs_2contributing.html#dynamic-content',1,'']]]
+];
---
+++ pr-preview/pr-511/search/variables_2.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['id_0',['id',['../structQDMI__Program__Format.html#a47110fa755629ce34822cca9c589ac4a',1,'QDMI_Program_Format::id'],['../structQDMI__Program__Feature.html#af352c3f325e3cae93c6f6ad99c604bd7',1,'QDMI_Program_Feature::id']]]
+];
