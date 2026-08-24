08f314d2aec9429746cb3f1937fba6779c9e4ae9
burgholzer <burgholzer@users.noreply.github.com>
2026-08-24 19:27:53 +0000

    Deploy preview for PR 512 🛫

---
+++ pr-preview/pr-512/dir_9cd947fdfc01699634c8a2ccc8255bf3.js
@@ -0,0 +1,4 @@
+var dir_9cd947fdfc01699634c8a2ccc8255bf3 =
+[
+    [ "client_version.h", "client__version_8h_source.html", null ]
+];
---
+++ pr-preview/pr-512/search/variables_1.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['encoding_0',['encoding',['../structQDMI__Program__Format.html#ab26c782006c8af9fab1710aa9c93b321',1,'QDMI_Program_Format']]]
+];
