645d53d4714b1a0e96dc5103dbbfa86c01860e6f
burgholzer <burgholzer@users.noreply.github.com>
2026-08-24 19:34:47 +0000

    Deploy preview for PR 513 🛫

---
+++ pr-preview/pr-513/files_dup.js
@@ -0,0 +1,9 @@
+var files_dup =
+[
+    [ "build", "dir_4fef79e7177ba769987a8da36c892c5f.html", "dir_4fef79e7177ba769987a8da36c892c5f" ],
+    [ "client.h", "client_8h.html", "client_8h" ],
+    [ "constants.h", "constants_8h.html", "constants_8h" ],
+    [ "device.h", "device_8h.html", "device_8h" ],
+    [ "export.h", "export_8h.html", null ],
+    [ "types.h", "types_8h.html", "types_8h" ]
+];
---
+++ pr-preview/pr-513/search/all_1a.js
@@ -0,0 +1,5 @@
+var searchData=
+[
+  ['value_0',['value',['../structQDMI__Program__Feature.html#a9a00d1c9c44c9c3e70c7912c5bbdea50',1,'QDMI_Program_Feature']]],
+  ['version_1',['version',['../structQDMI__Program__Format.html#aadcf574516b850840cc4352428416566',1,'QDMI_Program_Format']]]
+];
