a092c7f1e8aa937b7d5e5a7e54fc0f886326f6c3
denialhaag <denialhaag@users.noreply.github.com>
2026-09-10 19:24:02 +0000

    Deploy preview for PR 539 🛫

---
+++ pr-preview/pr-539/group__device__interface.js
@@ -0,0 +1,9 @@
+var group__device__interface =
+[
+    [ "QDMI Device Session Interface", "group__device__session__interface.html", "group__device__session__interface" ],
+    [ "QDMI Device Query Interface", "group__device__query__interface.html", "group__device__query__interface" ],
+    [ "QDMI Device Job Interface", "group__device__job__interface.html", "group__device__job__interface" ],
+    [ "QDMI_Child_Device", "group__device__interface.html#ga112235355550f0b2defb14e3e49323c4", null ],
+    [ "QDMI_device_initialize", "group__device__interface.html#ga99e892052f2a14ae1da53f171649abc2", null ],
+    [ "QDMI_device_finalize", "group__device__interface.html#gacea756e5f8d7cb2940252ed6d4e80571", null ]
+];
---
+++ pr-preview/pr-539/search/pages_4.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['5_20licensing_20and_20copyright_0',['5. Licensing and Copyright',['../md_docs_2ai__usage.html#licensing-and-copyright',1,'']]]
+];
