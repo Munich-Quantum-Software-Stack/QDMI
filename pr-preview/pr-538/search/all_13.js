eba6fd729c6507a4e283bd81d1e6f0fe2d35dfce
marcelwa <marcelwa@users.noreply.github.com>
2026-09-10 10:01:43 +0000

    Deploy preview for PR 538 🛫

---
+++ pr-preview/pr-538/search/all_13.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['of_20contributions_0',['Types of Contributions',['../md_docs_2contributing.html#types-of-contributions',1,'']]],
+  ['of_20jobs_20for_20devices_20and_20clients_1',['Why are there separate kinds of jobs for devices and clients?',['../md_docs_2rationale.html#rationale-job-structs',1,'']]],
+  ['of_20qdmi_2',['The structure of QDMI',['../md_docs_2rationale.html#rationale-structure',1,'']]],
+  ['of_20sites_3',['Why are there different kinds of sites?',['../md_docs_2rationale.html#rationale-site-types',1,'']]],
+  ['on_20source_20code_4',['Working on Source Code',['../md_docs_2contributing.html#working-on-source-code',1,'']]],
+  ['on_20the_20documentation_5',['Working on the Documentation',['../md_docs_2contributing.html#working-on-the-documentation',1,'']]],
+  ['opaque_20pointers_6',['Why does QDMI use opaque pointers?',['../md_docs_2rationale.html#rationale-opaque-pointers',1,'']]],
+  ['open_20source_7',['Sustainable Open Source',['../md_docs_2ai__usage.html#sustainable-open-source',1,'']]],
+  ['or_20enumeration_20properties_8',['Integer or Enumeration Properties',['../md_docs_2examples.html#device-int-enumeration',1,'']]]
+];
---
+++ pr-preview/pr-538/search/pages_3.js
@@ -0,0 +1,4 @@
+var searchData=
+[
+  ['4_20transparency_20and_20disclosure_0',['4. Transparency and Disclosure',['../md_docs_2ai__usage.html#transparency-and-disclosure',1,'']]]
+];
