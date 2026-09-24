3737fd0698390e8fa4931a3a7ee24a8f286195bd
burgholzer <burgholzer@users.noreply.github.com>
2026-09-24 17:15:12 +0000

    Deploy preview for PR 552 🛫

---
+++ pr-preview/pr-552/search/all_14.js
@@ -0,0 +1,12 @@
+var searchData=
+[
+  ['placed_20in_20the_20constants_20h_20header_20and_20some_20are_20not_0',['Why are some enum definitions placed in the &lt;span class=&quot;tt&quot;&gt;constants.h&lt;/span&gt; header and some are not?',['../md_docs_2rationale.html#rationale-enum-definitions',1,'']]],
+  ['pointers_1',['Why does QDMI use opaque pointers?',['../md_docs_2rationale.html#rationale-opaque-pointers',1,'']]],
+  ['prefix_2',['Why do device implementations use a prefix?',['../md_docs_2rationale.html#rationale-prefix',1,'']]],
+  ['principles_3',['Core Principles',['../md_docs_2ai__usage.html#core-principles',1,'']]],
+  ['prohibited_20uses_4',['Prohibited Uses',['../md_docs_2ai__usage.html#prohibited-uses',1,'']]],
+  ['project_5',['Creating a new Project',['../md_docs_2templates.html#template-create',1,'']]],
+  ['properties_6',['Properties',['../md_docs_2examples.html#device-string',1,'Basic String Properties'],['../md_docs_2examples.html#device-complex',1,'Complex Properties'],['../md_docs_2examples.html#device-int-enumeration',1,'Integer or Enumeration Properties'],['../md_docs_2examples.html#device-list',1,'List Properties']]],
+  ['property_7',['Why does QDMI not define individual functions for each property?',['../md_docs_2rationale.html#rationale-properties',1,'']]],
+  ['pull_20request_20workflow_8',['Pull Request Workflow',['../md_docs_2contributing.html#pull-request-workflow',1,'']]]
+];
---
+++ pr-preview/pr-552/search/all_16.js
@@ -0,0 +1,10 @@
+var searchData=
+[
+  ['rationale_0',['Rationale',['../md_docs_2rationale.html',1,'']]],
+  ['request_20workflow_1',['Pull Request Workflow',['../md_docs_2contributing.html#pull-request-workflow',1,'']]],
+  ['resources_2',['Further Links and Resources',['../md_docs_2contributing.html#further-links-and-resources',1,'']]],
+  ['responsible_3',['1. You are Responsible',['../md_docs_2ai__usage.html#you-are-responsible',1,'']]],
+  ['result_20ordering_4',['Result Ordering',['../md_docs_2examples.html#device-result-ordering',1,'']]],
+  ['running_20the_20c_20tests_20and_20code_20coverage_5',['Running the C++ Tests and Code Coverage',['../md_docs_2contributing.html#running-the-c-tests-and-code-coverage',1,'']]],
+  ['running_20the_20tests_6',['Building the Template and Running the Tests',['../md_docs_2templates.html#template-building',1,'']]]
+];
